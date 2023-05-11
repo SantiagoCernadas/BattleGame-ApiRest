d4a3ef7189956ba34960e71d1267436de5655325
santiago <santiagocernadas29@gmail.com>
2023-05-11 14:18:12 -0300

    creacion del servicio de personajes.

--- src/main/java/com/example/HeroGameBattleApi/Service/PersonajeService.java
+++ src/main/java/com/example/HeroGameBattleApi/Service/PersonajeService.java
@@ -1,5 +1,23 @@
 package com.example.HeroGameBattleApi.Service;
 
-public class PersonajeService {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.HeroGameBattleApi.Entity.Personaje;
+import com.example.HeroGameBattleApi.Repository.PersonajeRepository;
+
+@Service
+public class PersonajeService {
+	@Autowired
+	PersonajeRepository personajeRepository;
+	
+	public ArrayList<Personaje> obtenerPersonajes(){
+		return (ArrayList<Personaje>)personajeRepository.findAll();
+	}
+	
+	public Personaje guardarPersonaje(Personaje personaje) {
+		return personajeRepository.save(personaje);
+	}
 }
