f22b370cd5eabc014f7b64162d7cbf30689e2b00
santiago <santiagocernadas29@gmail.com>
2023-05-11 12:59:13 -0300

    se agrega el proyecto

---
+++ src/main/java/com/example/HeroGameBattleApi/HeroGameBattleApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.HeroGameBattleApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeroGameBattleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeroGameBattleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HeroGameBattleApi/HeroGameBattleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HeroGameBattleApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroGameBattleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
